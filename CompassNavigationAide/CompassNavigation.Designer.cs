c80733b52d2b179e2a275ff86bb00296dcbdd11a
Shane Feek <shane.feek@gmail.com>
2017-01-24 08:55:31 -0500

    First Commit

---
+++ CompassNavigationAide/CompassNavigation.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace CompassNavigationAide
+{
+    partial class CompassNavigation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CompassNavigation));
+            this.txtLatLong1 = new System.Windows.Forms.TextBox();
+            this.lblLatLong1 = new System.Windows.Forms.Label();
+            this.txtLatLong2 = new System.Windows.Forms.TextBox();
+            this.lblLatLong2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtStepLength = new System.Windows.Forms.TextBox();
+            this.lblStepLength = new System.Windows.Forms.Label();
+            this.cmbUnits = new System.Windows.Forms.ComboBox();
+            this.lblUnits = new System.Windows.Forms.Label();
+            this.bntWayPoint = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.txtMagDev = new System.Windows.Forms.TextBox();
+            this.lblMagDev = new System.Windows.Forms.Label();
+            this.txtComment = new System.Windows.Forms.TextBox();
+            this.lblComment = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLatLong1
+            // 
+            this.txtLatLong1.Location = new System.Drawing.Point(91, 12);
+            this.txtLatLong1.Name = "txtLatLong1";
+            this.txtLatLong1.Size = new System.Drawing.Size(266, 20);
+            this.txtLatLong1.TabIndex = 0;
+            // 
+            // lblLatLong1
+            // 
+            this.lblLatLong1.AutoSize = true;
+            this.lblLatLong1.Location = new System.Drawing.Point(12, 15);
+            this.lblLatLong1.Name = "lblLatLong1";
+            this.lblLatLong1.Size = new System.Drawing.Size(65, 13);
+            this.lblLatLong1.TabIndex = 1;
+            this.lblLatLong1.Text = "Lat,Long #1";
+            // 
+            // txtLatLong2
+            // 
+            this.txtLatLong2.Location = new System.Drawing.Point(91, 38);
+            this.txtLatLong2.Name = "txtLatLong2";
+            this.txtLatLong2.Size = new System.Drawing.Size(266, 20);
+            this.txtLatLong2.TabIndex = 1;
+            // 
+            // lblLatLong2
+            // 
+            this.lblLatLong2.AutoSize = true;
+            this.lblLatLong2.Location = new System.Drawing.Point(12, 41);
+            this.lblLatLong2.Name = "lblLatLong2";
+            this.lblLatLong2.Size = new System.Drawing.Size(65, 13);
+            this.lblLatLong2.TabIndex = 3;
+            this.lblLatLong2.Text = "Lat,Long #2";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(15, 202);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(364, 203);
+            this.txtOutput.TabIndex = 9;
+            this.txtOutput.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(263, 159);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtStepLength
+            // 
+            this.txtStepLength.Location = new System.Drawing.Point(91, 64);
+            this.txtStepLength.Name = "txtStepLength";
+            this.txtStepLength.Size = new System.Drawing.Size(100, 20);
+            this.txtStepLength.TabIndex = 2;
+            // 
+            // lblStepLength
+            // 
+            this.lblStepLength.AutoSize = true;
+            this.lblStepLength.Location = new System.Drawing.Point(12, 67);
+            this.lblStepLength.Name = "lblStepLength";
+            this.lblStepLength.Size = new System.Drawing.Size(65, 13);
+            this.lblStepLength.TabIndex = 12;
+            this.lblStepLength.Text = "Step Length";
+            // 
+            // cmbUnits
+            // 
+            this.cmbUnits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUnits.FormattingEnabled = true;
+            this.cmbUnits.Items.AddRange(new object[] {
+            "Feet",
+            "Meters"});
+            this.cmbUnits.Location = new System.Drawing.Point(91, 90);
+            this.cmbUnits.Name = "cmbUnits";
+            this.cmbUnits.Size = new System.Drawing.Size(86, 21);
+            this.cmbUnits.TabIndex = 4;
+            // 
+            // lblUnits
+            // 
+            this.lblUnits.AutoSize = true;
+            this.lblUnits.Location = new System.Drawing.Point(12, 93);
+            this.lblUnits.Name = "lblUnits";
+            this.lblUnits.Size = new System.Drawing.Size(31, 13);
+            this.lblUnits.TabIndex = 11;
+            this.lblUnits.Text = "Units";
+            // 
+            // bntWayPoint
+            // 
+            this.bntWayPoint.Location = new System.Drawing.Point(89, 159);
+            this.bntWayPoint.Name = "bntWayPoint";
+            this.bntWayPoint.Size = new System.Drawing.Size(102, 23);
+            this.bntWayPoint.TabIndex = 6;
+            this.bntWayPoint.Text = "Add Waypoint";
+            this.bntWayPoint.UseVisualStyleBackColor = true;
+            this.bntWayPoint.Click += new System.EventHandler(this.bntWayPoint_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(137, 420);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(107, 23);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Print Waypoints";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // txtMagDev
+            // 
+            this.txtMagDev.Location = new System.Drawing.Point(303, 64);
+            this.txtMagDev.Name = "txtMagDev";
+            this.txtMagDev.Size = new System.Drawing.Size(54, 20);
+            this.txtMagDev.TabIndex = 3;
+            // 
+            // lblMagDev
+            // 
+            this.lblMagDev.AutoSize = true;
+            this.lblMagDev.Location = new System.Drawing.Point(210, 67);
+            this.lblMagDev.Name = "lblMagDev";
+            this.lblMagDev.Size = new System.Drawing.Size(87, 13);
+            this.lblMagDev.TabIndex = 14;
+            this.lblMagDev.Text = "Mag. Declination";
+            // 
+            // txtComment
+            // 
+            this.txtComment.Location = new System.Drawing.Point(91, 117);
+            this.txtComment.Name = "txtComment";
+            this.txtComment.Size = new System.Drawing.Size(266, 20);
+            this.txtComment.TabIndex = 5;
+            // 
+            // lblComment
+            // 
+            this.lblComment.AutoSize = true;
+            this.lblComment.Location = new System.Drawing.Point(12, 120);
+            this.lblComment.Name = "lblComment";
+            this.lblComment.Size = new System.Drawing.Size(56, 13);
+            this.lblComment.TabIndex = 10;
+            this.lblComment.Text = "Comments";
+            // 
+            // CompassNavigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 452);
+            this.Controls.Add(this.lblComment);
+            this.Controls.Add(this.txtComment);
+            this.Controls.Add(this.lblMagDev);
+            this.Controls.Add(this.txtMagDev);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.bntWayPoint);
+            this.Controls.Add(this.lblUnits);
+            this.Controls.Add(this.cmbUnits);
+            this.Controls.Add(this.lblStepLength);
+            this.Controls.Add(this.txtStepLength);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblLatLong2);
+            this.Controls.Add(this.txtLatLong2);
+            this.Controls.Add(this.lblLatLong1);
+            this.Controls.Add(this.txtLatLong1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CompassNavigation";
+            this.Text = "Compass Navigation Aide v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLatLong1;
+        private System.Windows.Forms.Label lblLatLong1;
+        private System.Windows.Forms.TextBox txtLatLong2;
+        private System.Windows.Forms.Label lblLatLong2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtStepLength;
+        private System.Windows.Forms.Label lblStepLength;
+        private System.Windows.Forms.ComboBox cmbUnits;
+        private System.Windows.Forms.Label lblUnits;
+        private System.Windows.Forms.Button bntWayPoint;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.TextBox txtMagDev;
+        private System.Windows.Forms.Label lblMagDev;
+        private System.Windows.Forms.TextBox txtComment;
+        private System.Windows.Forms.Label lblComment;
+    }
+}
+
---
+++ CompassNavigationAide/CompassNavigation.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompassNavigationAide
+{
+    public partial class CompassNavigation : Form
+    {
+        int waypoint=1;
+        double totalDistance;
+
+        public CompassNavigation()
+        {
+            InitializeComponent();
+
+            txtMagDev.Text = "0";
+            txtStepLength.Text = "2.5";
+            cmbUnits.SelectedIndex = 0;
+            totalDistance = 0.0;
+        }
+
+        // Clear the waypoints
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLatLong1.Text = String.Empty;
+            txtLatLong2.Text = String.Empty;
+            txtOutput.Text = String.Empty;
+            txtComment.Text = String.Empty;
+            waypoint = 1;
+            totalDistance = 0.0;
+        }
+
+        // Calculate and Add a waypoint
+        private void bntWayPoint_Click(object sender, EventArgs e)
+        {
+            double lat1; 
+            double lat2; 
+            double long1;
+            double long2;
+            double magDev;
+            double paces;
+
+            try
+            {
+                string[] latlong1 = txtLatLong1.Text.Split(',');
+                string[] latlong2 = txtLatLong2.Text.Split(',');
+
+                lat1 = Convert.ToDouble(latlong1[0]);
+                lat2 = Convert.ToDouble(latlong2[0]);
+                long1 = Convert.ToDouble(latlong1[1]);
+                long2 = Convert.ToDouble(latlong2[1]);
+                magDev = Convert.ToDouble(txtMagDev.Text);
+                paces = Convert.ToDouble(txtStepLength.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Latitude and Longitude entries!");
+                return;
+            }
+
+            double distance = CalculateDistance(lat1, long1, lat2, long2);
+            totalDistance += distance;
+            double bearing = CalculateBearing(lat1, long1, lat2, long2);
+
+            txtOutput.AppendText("\r\n\r\n\r\n" + String.Format("******* Way Point {0} *******", waypoint));
+            txtOutput.AppendText("\r\n" + txtLatLong1.Text + "  To  " + txtLatLong2.Text);
+
+            txtOutput.AppendText("\r\n\r\nComments: " + txtComment.Text);
+
+            if (cmbUnits.SelectedIndex == 0)
+            {
+                txtOutput.AppendText(String.Format("\r\nDistance: {0:0.0} Feet", distance * 3.28084));
+                txtOutput.AppendText(String.Format(" / {0:0.000} Miles", distance * 3.28084 / 5280.0));
+                txtOutput.AppendText(String.Format(" / {0:0} Paces", distance * 3.28084 / paces));
+            }
+            else
+            {
+                txtOutput.AppendText(String.Format("\r\n\r\nDistance: {0:0.0} Meters", distance));
+                txtOutput.AppendText(String.Format(" / {0:0.000} Kilometers", distance / 1000.0));
+                txtOutput.AppendText(String.Format(" / {0:0} Paces", distance / paces));
+            }
+
+            txtOutput.AppendText(String.Format("\r\nBearing:  True {0:0.0} Degrees / ", bearing ));
+            txtOutput.AppendText(String.Format("Magnetic {0:0.0} Degrees", bearing + magDev));
+
+            if (cmbUnits.SelectedIndex == 0)
+            {
+                txtOutput.AppendText(String.Format("\r\nAccumulated Distance: {0:0.0} Feet", totalDistance * 3.28084));
+                txtOutput.AppendText(String.Format(" / {0:0.000} Miles", totalDistance * 3.28084 / 5280.0));
+                txtOutput.AppendText(String.Format(" / {0:0} Paces", totalDistance * 3.28084 / paces));
+            }
+            else
+            {
+                txtOutput.AppendText(String.Format("\r\nAccumulated Distance: {0:0.0} Meters", totalDistance));
+                txtOutput.AppendText(String.Format(" / {0:0.000} Kilometers", totalDistance / 1000.0));
+                txtOutput.AppendText(String.Format(" / {0:0} Paces", totalDistance / paces));
+            }
+
+            txtLatLong1.Text = txtLatLong2.Text;
+            txtLatLong2.Text = String.Empty;
+
+            waypoint++;
+            txtComment.Text = String.Empty;
+        }
+
+        // Print our stuff
+        private void PrintDocumentOnPrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(this.txtOutput.Text, this.txtOutput.Font, Brushes.Black, 10, 25);
+        }
+
+        // Calculate the heading
+        private double CalculateDistance (double latitude1, double longitude1, double latitude2, double longitude2)
+        {
+            double R = 6371e3;
+            double lat1 = DegreeToRadian(latitude1);
+            double lat2 = DegreeToRadian(latitude2);
+            double deltaLat = DegreeToRadian(latitude2 - latitude1);
+            double deltaLong = DegreeToRadian(longitude2 - longitude1);
+
+            double a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Sin(deltaLong / 2) * Math.Sin(deltaLong / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double d = R * c;
+
+            return d;
+        }
+
+        // Calculate bearing
+        private double CalculateBearing(double latitude1, double longitude1, double latitude2, double longitude2)
+        {
+            double lat1 = DegreeToRadian(latitude1);
+            double lat2 = DegreeToRadian(latitude2);
+            double long1 = DegreeToRadian(longitude1);
+            double long2 = DegreeToRadian(longitude2);
+
+            double y = Math.Sin(long2 - long1) * Math.Cos(lat2);
+            double x = Math.Cos(lat1) * Math.Sin(lat2) - Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(long2 - long1);
+
+            double bearing = RadianToDegree(Math.Atan2(y, x));
+
+            // Correct "wrap around" in necessary
+            if (bearing < 0) bearing = 360.0 + bearing;
+            if (bearing > 360.0) bearing = bearing - 360.0;
+
+            return bearing;
+        }
+
+        // Convert Degrees to Radians
+        private double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        // Convert Radians to Degrees
+        private double RadianToDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
+        // Send waypoints to the printer
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += PrintDocumentOnPrintPage;
+            printDocument.Print();
+        }
+    }
+}
